b653914bcc38c4267d64adcd45968693c57fb6f8
Hiep Nguyen Duc <mrhiep314@gmail.com>
2023-12-22 02:08:05 +0700

    Commit

--- deployment/docker/publish.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-echo "$DOCKER_PASSWORD"
-echo "$DOCKER_USERNAME"
-
-docker-compose -f deployment/docker/docker-compose-build.yaml push
